c93772bac0c1fab252edf578f72cb91e2d4d56c5
Daniel Alvarenga Lima <danielalvarengalima@gmail.com>
2022-05-27 19:05:49 -0300

    First route and starting a web server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from SnippetBox"))
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// Servemux catch all requests for the URL pattern "/",
+	// even trying access other patterns like "/any"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// The function http.ListenAndServe() should receive the TCP network
+	// in the format "host:port" to specifics host and port number or
+	// ":port" to any host and specific port number or
+	// ":my-port-name" to use named port that Go will try to get the
+	// correspondence from "/etc/services"
+	log.Println("Stating server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }

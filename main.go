b23802fca8c05e0e810f6b6176e7990fc5574231
Daniel Alvarenga Lima <danielalvarengalima@gmail.com>
2022-06-29 18:44:33 -0300

    fmt.Fprintf to write api response and getting query params from url

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +19,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Fprintf can receive any instance that implements the io.Writer interface
+	// working as a helper for write responses
+	fmt.Fprintf(w, "Display a specific snippet with id %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {

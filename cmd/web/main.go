29b09f3b17014808363b1037f654a2dc8d0574fc
Daniel Alvarenga Lima <danielalvarengalima@gmail.com>
2022-07-08 18:31:05 -0300

    Serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,6 +6,28 @@ import (
 )
 
 func main() {
+	// * Instead explicitly declare a servemux and use mux.HandleFunc(),
+	// you can use http.HandleFunc() directly to set routes. It uses the global variable
+	// "DefaultServeMux" implicitly created by "net/http" package, but it isn't safe
+	// because any package can access global variables and handle this in your application.
+	mux := http.NewServeMux()
+
+	// * The Go file server serve static files
+	// * Automatically sanitises paths (path.Clean()) to avoid directory traversal attacks
+	// * Supports range requests to download large files
+	// * If the file wasn't modified since the last request, it returns a "304 Not Modified"
+	// status code instead the file
+	// * If Content-Type is setted wrong (mime.TypeByExtension()) you can add your custom
+	// type using the function mime.AddExtensionType()
+	// * How to disable directory listing:
+	// https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings
+	// * To serve a single file direct from some handler function you can use http.ServeFile(), but
+	// need to use filepath.Clean() to sanitize the path
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// Create the route for static files removing the "/static" prefix
+	// before the request reaches the file Server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Servemux supports two types of URL patterns:
 	// * Subtree paths: it ends with trailing slash, like "/" or "/public/", and
 	// catch any URl path starting with the pattern, like a wildcard ("/**" or "/public/**")
@@ -21,11 +43,6 @@ func main() {
 	// * Request without trailing slash that matches with some subtree path is redirected
 	// (301 Permanent Redirect) to subtree path. Ex: "/any" to "/any/"
 	// * URL pattenrs accepts host names like "foo.any.com/create"
-	// * Instead explicitly declare a servemux and use mux.HandleFunc(),
-	// you can use http.HandleFunc() directly. It uses the global variable "DefaultServeMux"
-	// implicitly created by "net/http" package, but it isn't safe because any package
-	// can access global variables and handle this in your application.
-	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
